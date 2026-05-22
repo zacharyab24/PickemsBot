2615d44cf95b3c445daeee9088d9b01f31f930d3
zacharyab24 <zacbower0@gmail.com>
2026-05-22 23:52:00 +1000

    refactor: move wikitext code to the config package where it belongs

--- api/external/external.go
+++ api/external/external.go
@@ -7,65 +7,11 @@
 package external
 
 import (
-	"compress/gzip"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 )
-
-// GetWikitext fetches raw wikitext from a given URL. This function does not perform any parsing on the text.
-// It receives string that contains URL for liquipedia page we wish to parse
-// (e.g. https://liquipedia.net/counterstrike/PGL/2024/Copenhagen/Opening_Stage?action=raw).
-// It returns string containing raw wiki text and errors.
-func GetWikitext(url string) (string, error) {
-
-	// Create HTTP Request
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Failed to create request", err)
-	}
-
-	// Headers to apply with API requirements
-	request.Header.Set("User-Agent", "LiquipediaDataFetcher/1.0")
-	request.Header.Set("Accept-Encoding", "gzip")
-
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println("Request failed: ", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		fmt.Printf("Failed to fetch page. Status code: %d\n", response.StatusCode)
-		return "", err
-	}
-
-	// Get wiki text from response
-	var body []byte
-	if response.Header.Get("Content-Encoding") == "gzip" {
-		reader, err := gzip.NewReader(response.Body)
-		if err != nil {
-			fmt.Println("Failed to create gzip reader: ", err)
-			return "", err
-		}
-		defer reader.Close()
-		body, err = io.ReadAll(reader)
-		if err != nil {
-			return "", err
-		}
-	} else {
-		body, err = io.ReadAll(response.Body)
-	}
-
-	if err != nil {
-		fmt.Println("Failed to read response body:", err)
-		return "", err
-	}
-
-	return string(body), err
-}
 
 // GetLiquipediaMatchDataByPage fetches all match data for a tournament page from
 // the LiquipediaDB /match endpoint using a [[pagename::X]] condition. This
